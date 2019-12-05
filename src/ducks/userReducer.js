3790e67e09a4de518109757b1a6fab7642739a32
Kevin Montero <kevinwilding1997@gmail.com>
2019-12-05 16:33:20 -0700

    Done

---
+++ src/ducks/userReducer.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+
+const initialState = {
+	email: null,
+	firstName: null,
+    lastName: null,
+    errorMessage: ''
+};
+
+const REQUEST_USER_DATA = 'REQUEST_USER_DATA';
+
+export const requestUserData = () => {
+	let data = axios
+		.get('/auth/user-data')
+		.then(res => res.data)
+        .catch(err => err.message);
+        
+	return {
+		type: REQUEST_USER_DATA,
+		payload: data
+	};
+};
+
+export default function userReducer(state = initialState, action) {
+	const { type, payload } = action;
+
+	switch (type) {
+		case REQUEST_USER_DATA + '_FULFILLED':
+			// const { email, firstName, lastName } = payload.user;
+            return { ...state };
+        case REQUEST_USER_DATA + '_REJECTED':
+                return {...state, errorMessage: payload};
+		default:
+			return state;
+	}
+}
+
+// import axios from 'axios';
+
+// const initialState = {
+//   email: null,
+//   firstName: null,
+//   lastName: null
+// };
+
+// const REQUEST_USER_DATA = 'REQUEST_USER_DATA'
+
+// export const requestUserData = () => {
+//   let data = axios.get('/auth/user-data').then(res => res.data)
+//   return {
+//     type: REQUEST_USER_DATA,
+//     payload: data
+//   }
+// }
+
+// export default function (state = initialState, action) {
+//   switch (action.type) {
+//     case REQUEST_USER_DATA + '_FULFILLED':
+//       const { email, firstName, lastName } = action.payload.user
+//       return { email, firstName, lastName };
+//     default:
+//       return state;
+//   }
+// }
