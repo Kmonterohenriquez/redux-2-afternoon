8928a3922280b9c361dd1423f54f6576c06bbcc9
Kevin Montero <kevinwilding1997@gmail.com>
2019-12-05 23:57:22 -0700

    removing some comments

--- src/ducks/userReducer.js
+++ src/ducks/userReducer.js
@@ -34,31 +34,3 @@ export default function userReducer(state = initialState, action) {
 			return state;
 	}
 }
-
-// import axios from 'axios';
-
-// const initialState = {
-//   email: null,
-//   firstName: null,
-//   lastName: null
-// };
-
-// const REQUEST_USER_DATA = 'REQUEST_USER_DATA'
-
-// export const requestUserData = () => {
-//   let data = axios.get('/auth/user-data').then(res => res.data)
-//   return {
-//     type: REQUEST_USER_DATA,
-//     payload: data
-//   }
-// }
-
-// export default function (state = initialState, action) {
-//   switch (action.type) {
-//     case REQUEST_USER_DATA + '_FULFILLED':
-//       const { email, firstName, lastName } = action.payload.user
-//       return { email, firstName, lastName };
-//     default:
-//       return state;
-//   }
-// }
